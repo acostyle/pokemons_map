888391a550be70b071443c252430f99c32408240
Medet Mussayev <acostyle.m.z@protonmail.com>
2021-03-15 20:16:23 +0600

    Connect models to main page

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -4,6 +4,8 @@ import json
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
+from .models import PokemonEntity, Pokemon
+
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = "https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832&fill=transparent"
@@ -32,11 +34,11 @@ def show_all_pokemons(request):
                 folium_map, pokemon_entity['lat'], pokemon_entity['lon'], pokemon['img_url'])
 
     pokemons_on_page = []
-    for pokemon in pokemons:
+    for pokemon in Pokemon.objects.all():
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': pokemon.picture.url if pokemon.picture else None,
+            'title_ru': pokemon.title,
         })
 
     return render(request, "mainpage.html", context={
